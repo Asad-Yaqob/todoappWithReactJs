2fd7c22f063d05723728ae48a486335dbb82c6cc
AsadYaqoob <asadmuhammad09123@gmail.com>
2024-11-11 12:59:26 +0500

    image-folder-created

--- src/components/NoITem.jsx
+++ src/components/NoITem.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const NoITem = () => {
   return (
-      <div className='flex flex-col items-center'>
-        <img src="./public/empty-list.png" className='w-20 m-2' alt="" />
-        <div>
-            <p>Todo list is empty</p>
-        </div>
+    <div className="flex flex-col items-center">
+      <img src="/images/empty-list.png" className="w-20 m-2" alt="" />
+      <div>
+        <p>Todo list is empty</p>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default NoITem
+export default NoITem;
