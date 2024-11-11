4d172595021061f648af157fe56a5a8a097ef874
AsadYaqoob <asadmuhammad09123@gmail.com>
2024-11-11 12:53:26 +0500

    correct-image-path

--- src/components/NoITem.jsx
+++ src/components/NoITem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NoITem = () => {
   return (
       <div className='flex flex-col items-center'>
-        <img src="../src/assets/empty-list.png" className='w-20 m-2' alt="" />
+        <img src="./public/empty-list.png" className='w-20 m-2' alt="" />
         <div>
             <p>Todo list is empty</p>
         </div>
